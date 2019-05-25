f37a220c61492986fc23ccc43366e95525fd1ec7
Ray Lee <ray.lee@lyrasis.org>
2019-05-24 21:37:48 -0700

    DRYD-655: Upgrade to babel 7 and webpack 4.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import messages from './messages';
 import plugins from './plugins';
 import styles from '../styles/cspace-ui-plugin-profile/anthro.css';
 
-module.exports = () => ({
+export default () => ({
   messages,
   className: styles.common,
   prettyUrls: true,
