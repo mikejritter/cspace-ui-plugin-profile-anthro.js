2468a277cb6fe060d24cf6eef4f19b5c4f811071
Michael Ritter <mike.ritter@lyrasis.org>
2024-09-10 16:09:35 -0600

    DRYD-1503: Add production location verbatim

--- src/plugins/recordTypes/collectionobject/fields.js
+++ src/plugins/recordTypes/collectionobject/fields.js
@@ -843,6 +843,34 @@ export default (configContext) => {
           },
         },
       },
+      'ns2:collectionobjects_objectprod_extension': {
+        [config]: {
+          service: {
+            ns: 'http://collectionspace.org/services/collectionobject/domain/objectprod_extension',
+          },
+        },
+        objectProductionLocationsVerbatim: {
+          [config]: {
+            view: {
+              type: CompoundInput,
+            },
+          },
+          objectProductionLocationVerbatim: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.collectionobjects_objectprod.objectProductionLocationVerbatim.name',
+                  defaultMessage: 'Production location (verbatim)',
+                },
+              }),
+              repeating: true,
+              view: {
+                type: TextInput,
+              },
+            },
+          },
+        },
+      },
       ...extensions.culturalcare.collectionobject.fields,
       ...extensions.nagpra.collectionobject.fields,
       ...extensions.naturalhistory.collectionobject.fields,
