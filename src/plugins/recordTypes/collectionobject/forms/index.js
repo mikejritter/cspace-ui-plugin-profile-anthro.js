795656e0b0a03c8fd7cdd7c76074d0377f48c8b2
Michael Ritter <mike.ritter@lyrasis.org>
2024-09-26 15:26:25 -0600

    DRYD-1501: Add core NAGPRA template

--- src/plugins/recordTypes/collectionobject/forms/index.js
+++ src/plugins/recordTypes/collectionobject/forms/index.js
@@ -1,7 +1,9 @@
+import corenagpraForm from './corenagpra';
 import defaultForm from './default';
 import publicForm from './public';
 
 export default (configContext) => ({
+  corenagpra: corenagpraForm(configContext),
   default: defaultForm(configContext),
   public: publicForm(configContext),
   timebased: {
